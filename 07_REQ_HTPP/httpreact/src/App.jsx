3dd8371816b4edf977b757fa8c8cc420222d8c1a
narakushi <taina.santana1305@gmail.com>
2026-03-03 12:55:06 -0300

    :sparkles: aual 93 - resgatando dados com react

--- 07_REQ_HTPP/httpreact/src/App.jsx
+++ 07_REQ_HTPP/httpreact/src/App.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
+const url = "http://localhost:3000/products"
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+
+  useEffect(() => {
+    async function fetchData() {
+
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+
+  }, [])
+
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
