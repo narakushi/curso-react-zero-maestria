7067e8042593d7affa64998f6fac4cd57b551329
narakushi <taina.santana1305@gmail.com>
2026-03-07 15:18:28 -0300

    :sparkles: feat: aula 94 - adicionando dados com react

--- 07_REQ_HTPP/httpreact/src/App.jsx
+++ 07_REQ_HTPP/httpreact/src/App.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-const url = "http://localhost:3000/products"
+const url = "http://localhost:3000/products";
 
 function App() {
-
   const [products, setProducts] = useState([]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
 
   useEffect(() => {
     async function fetchData() {
-
       const res = await fetch(url);
 
       const data = await res.json();
@@ -20,9 +21,28 @@ function App() {
     }
 
     fetchData();
+  }, []);
 
-  }, [])
+  // 2  - add de produtos
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    //* IDENTIFICAMOS O TIPO DE REQUISIÇÃO PARA TODOS
+    //* OS VERBOS HTTP, MAS PARA GET É OPCIONAL
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
 
   return (
     <div className="App">
@@ -30,11 +50,39 @@ function App() {
 
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
