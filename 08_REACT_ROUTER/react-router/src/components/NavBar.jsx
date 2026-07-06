97ed452f90b28efc7281193fcfd955ad9f3b5421
narakushi <taina.santana1305@gmail.com>
2026-07-06 11:59:03 -0300

    :sparkles: feat: aula 112 - link aitvo

--- 08_REACT_ROUTER/react-router/src/components/NavBar.jsx
+++ 08_REACT_ROUTER/react-router/src/components/NavBar.jsx
@@ -1,13 +1,32 @@
 // 2 - links com react router
 import "./NavBar.css";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+
+      {/* Uso do isActive do NavLink */}
+      {/* <NavLink
+        to="/"
+        className={(isActive) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={(isActive) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Sobre
+      </NavLink> */}
+
+      {/* Aproveitando a inserção "automatica" do .active*/}
+
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
   );
 };
