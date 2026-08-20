8eaea8006e50cbcad38cc7e7ec4af2ff2032fca6
narakushi <taina.santana1305@gmail.com>
2026-08-20 09:21:25 -0300

    aula 118 - 120 - Criando e mudando contexto

--- 08_CONTEXT_API/context-api/src/main.jsx
+++ 08_CONTEXT_API/context-api/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { CounterContextProvider } from "./context/CounterContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* 2 - criar provider */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </StrictMode>,
-)
+);
--- 08_CONTEXT_API/context-api/src/pages/Home.jsx
+++ 08_CONTEXT_API/context-api/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { counter } = useContext(CounterContext);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      {/** 3 - alterando valor contexto */}
+      <ChangeCounter />
+    </div>
+  );
 };
 
 export default Home;
