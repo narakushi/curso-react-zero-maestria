86045b3d02f2750b129aefe2a61bd8c625baeaf9
narakushi <taina.santana1305@gmail.com>
2026-09-11 16:20:50 -0300

    :sparkles: feat: aula 129 - configurando o React Router

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
+function App() {
   return (
-    <>
-      <h1>Miniblog</h1>
-    </>
+    <div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<About />} path="/about" />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
