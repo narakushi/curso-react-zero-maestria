ffdceca383b5493436bbff5e9ffb3017e7b499b2
narakushi <taina.santana1305@gmail.com>
2026-09-12 10:29:05 -0300

    :sparkles: feat: aula 130 - adicionando componente de navbar

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -3,17 +3,21 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<About />} path="/about" />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ 10_MINIBLOG/miniblog/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini blog &copy; 2026</p>
+    </footer>
+  );
+};
+
+export default Footer;
