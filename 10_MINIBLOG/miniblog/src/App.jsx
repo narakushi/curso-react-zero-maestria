e5f82090348cca910f9f49c62986199f3b8b4c68
narakushi <taina.santana1305@gmail.com>
2026-09-15 16:52:00 -0300

    :sparkles: feat: aulas 133 e 134 - criando e estilizando o formulario

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -15,6 +18,8 @@ function App() {
           <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<About />} path="/about" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
           </Routes>
         </div>
         <Footer />
